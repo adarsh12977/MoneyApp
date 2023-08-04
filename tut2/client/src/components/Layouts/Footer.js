2e70b9cbd83d933f611b3fdd8a485ad8c15f71db
adarsh12977 <114898953+adarsh12977@users.noreply.github.com>
2023-08-05 01:49:42 +0530

    Add files via upload

---
+++ tut2/client/src/components/Layouts/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-3">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; adarsh
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
